1edaed0eb88b4ce24989f66de250d0740887a603
Anton Antonov <anton.synd.antonov@gmail.com>
2019-04-23 21:13:46 +0300

    Init

---
+++ cmd/example.go
@@ -0,0 +1,12 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/syndbg/gomodmyrepo/mig"
+)
+
+
+func PrintExample() {
+        fmt.Println(mig.Example)
+}
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/syndbg/gomodanotherrepo
+
+go 1.12
+
+require github.com/syndbg/gomodmyrepo/mig v1.0.0
